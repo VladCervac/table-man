0e73e1a740544c576186fdba5e7656b2ffcdbf57
VladCervac <60219087+VladCervac@users.noreply.github.com>
2021-11-26 08:55:07 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DataTable from "react-data-table-component";
+import Card from "@material-ui/core/Card";
+import SortIcon from "@material-ui/icons/ArrowDownward";
+import movies from "./movies";
+import "./styles.css";
+
+const columns = [
+  {
+    name: "Title",
+    selector: "title",
+    sortable: true
+  },
+  {
+    name: "Directior",
+    selector: "director",
+    sortable: true
+  },
+  {
+    name: "Runtime (m)",
+    selector: "runtime",
+    sortable: true,
+    right: true
+  }
+];
+
+function App() {
+  return (
+    <div className="App">
+      <Card>
+        <DataTable
+          title="Movies"
+          columns={columns}
+          data={movies}
+          defaultSortField="title"
+          sortIcon={<SortIcon />}
+          pagination
+          selectableRows
+        />
+      </Card>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
